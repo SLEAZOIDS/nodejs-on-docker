9f5e427579c3c2d47304012ebcc058854e594651
SLEAZOIDS <vegetarian304@gmail.com>
2018-07-15 23:29:38 +0900

    riot追加

---
+++ ejs/public/js/human.js
@@ -0,0 +1,3 @@
+riot.tag2('human', '<h1>{name}</h1>', '', '', function(opts) {
+    this.name = 'John';
+});
---
+++ ejs/public/tags/human.tag
@@ -0,0 +1,6 @@
+<human>
+  <h1>{name}</h1>
+  <script>
+    this.name = 'John';
+  </script>
+</human>
